dd2d1188244697b55814d7820d619f8d50335cd4
jjvaler1 <156548105+jjvaler1@users.noreply.github.com>
2025-02-25 14:48:45 -0700

    Update and rename js.script to script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById ("firstPara").innerHTML += 
+"<span class'tan'></span>";
+
+
+
+let images = document.getElementByIdTagName("img");
+console.log(images);
+for(let image of images){
+    image.width = "250px"; 
+}
+
+// third section 
+let lightBluespans = document.getElementsByClassName("light-blue");
+let dkBluespans = document.getElementsByClassName("mid-blue");
+console.log(lightBlueSpans);
+
+for(let span of lightBlueSpans){
+span.style.color = "#7C9EA6";
+    
+}
+
+for(let span of lightBlueSpans){
+    span.style.color = "3C5E73";
+  
+    }
+
+let svgs = document.querySelectorAll("#svgs svg");
+let colors = ("#283040", "#3CSE73", "#7C9EA6", "D98BCA", "F2DCC9");
+
+for (let i = 0; i < svgs.length; i++) {
+    svgs [i].style.stroke = colors [i];
+}
+
+document.querySelector("query .bold").style.color = "#7C9EA6";
